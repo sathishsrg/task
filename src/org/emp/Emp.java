37a12be56925b0ceebe2d0cbf1bee7432b58c9f2
“sathishsrg” <sathishsrgeee@gmail.com>
2023-02-11 19:36:20 +0530

    “massage”

---
+++ src/org/emp/Emp.java
@@ -0,0 +1,16 @@
+package org.emp;
+
+public class Emp {
+
+	public static void main(String[] args) {
+		
+		
+		String s="Welcome to java class";
+	System.out.println(s.substring(0, 7));
+	}
+}
+		
+		
+
+
+	
